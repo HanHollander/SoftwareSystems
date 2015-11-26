39bdeca435d1e5aed70c9e3786b30490d451fbc2
Han Hollander <han_hollander@hotmail.nl>
2015-11-26 14:36:30 +0100

    P3.3 af

--- softwaresystems/src/ss/week3/pw/StrongChecker.java
+++ softwaresystems/src/ss/week3/pw/StrongChecker.java
@@ -2,8 +2,17 @@ package ss.week3.pw;
 
 public class StrongChecker extends BasicChecker{
 	
-	public strongAcceptable(String suggestion) {
-		return (super.acceptable(suggestion) && suggestion.charAt(0) == 1);
+	public boolean strongAcceptable(String suggestion) {
+		return super.acceptable(suggestion) && firstCharIsLetter(suggestion) && lastCharIsNumber(suggestion);
 	}
-
+	
+	public boolean firstCharIsLetter(String suggestion) {
+		int c = (int)suggestion.charAt(0);
+		return (c >= 65 && c <= 90) || (c >= 97 && c <= 122);
+	}
+	
+	public boolean lastCharIsNumber(String suggestion) {
+		int c = (int)suggestion.charAt(suggestion.length() - 1);
+		return (c >= 48 && c <= 57);
+	}
 }
