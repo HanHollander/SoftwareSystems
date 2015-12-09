c9a9492b92cc455689957bdf1a2371bb98514746
Han Hollander <han_hollander@hotmail.nl>
2015-12-09 12:51:29 +0100

    ss.week5.ttt

---
+++ softwaresystems/src/ss/week5/tictactoe/TicTacToe.java
@@ -0,0 +1,17 @@
+package ss.week5.tictactoe;
+
+/**
+ * Executable class for the game Tic Tac Toe. The game can be played against the
+ * computer. Lab assignment Module 2
+ * 
+ * @author Theo Ruys
+ * @version $Revision: 1.4 $
+ */
+public class TicTacToe {
+    public static void main(String[] args) {
+    	Player p1 = new HumanPlayer(args[0], Mark.OO); // "Wijtse", Mark.OO);
+    	Player p2 = new HumanPlayer(args[1], Mark.XX); // "Han", Mark.XX);
+        Game game = new Game(p1, p2);
+        game.start();
+    }
+}
