422c5bba991e1a753b5440e6b2ebf254770a7169
Wijtse Rekker <wijtse@rekkerit.nl>
2015-12-11 12:49:04 +0100

    SSS

--- softwaresystems/src/ss/week5/tictactoe/SuperSmartStrategy.java
+++ softwaresystems/src/ss/week5/tictactoe/SuperSmartStrategy.java
@@ -1,5 +1,8 @@
 package ss.week5.tictactoe;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class SuperSmartStrategy implements Strategy {
 
 	
@@ -9,7 +12,45 @@ public class SuperSmartStrategy implements Strategy {
 
 	
 	public int determineMove(Board b, Mark m) {
-		return 0;
+		int result = -1;
+		boolean determined = false;
+		Set<Integer> emptyFields = new HashSet<Integer>();
+		for (int i = 0; i < b.DIM * b.DIM; i++) {
+			if (b.isEmptyField(i)) {
+				emptyFields.add(i);
+			}
+		}
+		if (!determined){						// If instant win available
+			for (int field : emptyFields) {
+				Board copy = b.deepCopy();
+				copy.setField(field, m);
+				if (copy.gameOver()) {
+					result = field;
+					determined = true;
+				}
+			}
+		}
+		if (!determined) {						// If instant lose available
+			Mark other;
+			if (m == Mark.OO) {
+				other = Mark.XX;
+			} else {
+				other = Mark.OO;
+			}
+			for (int field : emptyFields) {
+				Board copy = b.deepCopy();
+				copy.setField(field, other);
+				if (copy.gameOver()) {
+					result = field;
+					determined = true;
+				}
+			}
+		} 
+		if (!determined){
+			for (int field : emptyFields) {
+				
+			}
+		}
+		return result;
 	}
-
 }
